1375107dd42df77fcfa314be4333e4c83fd2908e
DaviLiira <davilira444@gmail.com>
2023-11-30 22:16:18 -0300

    Melhorando o codigo utilizando classes e métodos

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-let heroi = {
-    nome: "José",
-    idade: 23,
-    tipo: {
-        0: ["mago", "magia"],
-        1: ["guerreiro", "espada"],
-        2: ["monge", "artes marciais"],
-        3: ["ninja", "shuriken"]
+class classeDoHeroi {
+  constructor(tipoClasse) {
+    this.tipoClasse = tipoClasse;
+  }
+  ataque() {
+    let tipoAtaque = "";
+    switch (true) {
+      case this.tipoClasse == "mago":
+        tipoAtaque = "magia";
+        console.log(`O ${this.tipoClasse} atacou utilizando ${tipoAtaque}`);
+        break;
+      case this.tipoClasse == "guerreiro":
+        tipoAtaque = "espada";
+        console.log(`O ${this.tipoClasse} atacou utilizando ${tipoAtaque}`);
+        break;
+      case this.tipoClasse == "monge":
+        tipoAtaque = "artes marciais";
+        console.log(`O ${this.tipoClasse} atacou utilizando ${tipoAtaque}`);
+        break;
+      case this.tipoClasse == "ninja":
+        tipoAtaque = "shuriken";
+        console.log(`O ${this.tipoClasse} atacou utilizando ${tipoAtaque}`);
+        break;
     }
+  }
 }
 
-function ataque(heroi) {
-    for(let index in heroi.tipo){
-        let [tipoClasse, tipoAtaque] = heroi.tipo[index]
-        console.log(`O ${tipoClasse} atacou usando ${tipoAtaque}`)
-    }
-}
+let heroiMago = new classeDoHeroi("mago");
+let heroiGuerreiro = new classeDoHeroi("guerreiro");
+let heroininja = new classeDoHeroi("ninja");
+let heroiMonge = new classeDoHeroi("monge");
 
-ataque(heroi)
+heroiMago.ataque();
+heroiGuerreiro.ataque();
+heroininja.ataque();
+heroiMonge.ataque();
